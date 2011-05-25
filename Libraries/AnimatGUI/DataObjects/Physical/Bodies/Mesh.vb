6f3c896189059671962276b81922b560b3b9cb38
David Cofer <dcofer@NeuroRoboticTech.com>
2011-05-25 11:33:24 +0000

    #56 comment: Added the mesh GUI object. Not tested yet.

---
+++ Libraries/AnimatGUI/DataObjects/Physical/Bodies/Mesh.vb
@@ -0,0 +1,109 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Xml
+Imports Crownwood.Magic.Common
+Imports AnimatGuiCtrls.Controls
+Imports Crownwood.Magic.Docking
+Imports Crownwood.Magic.Menus
+Imports AnimatGUI.Framework
+
+Namespace DataObjects.Physical.Bodies
+
+    Public Class Mesh
+        Inherits Physical.RigidBody
+
+#Region " Attributes "
+
+#End Region
+
+#Region " Properties "
+
+        Public Overrides ReadOnly Property WorkspaceImageName() As String
+            Get
+                Return "AnimatGUI.Box_Treeview.gif"
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property ButtonImageName() As String
+            Get
+                Return "AnimatGUI.Box_Button.gif"
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property Type() As String
+            Get
+                Return "Mesh"
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property PartType() As System.Type
+            Get
+                Return GetType(AnimatGUI.DataObjects.Physical.Bodies.Mesh)
+            End Get
+        End Property
+
+#End Region
+
+        Public Sub New(ByVal doParent As Framework.DataObject)
+            MyBase.New(doParent)
+            m_strDescription = ""
+
+        End Sub
+
+        Public Overrides Function Clone(ByVal doParent As Framework.DataObject, ByVal bCutData As Boolean, ByVal doRoot As Framework.DataObject) As Framework.DataObject
+            Dim oNewNode As New Bodies.Mesh(doParent)
+            oNewNode.CloneInternal(Me, bCutData, doRoot)
+            If Not doRoot Is Nothing AndAlso doRoot Is Me Then oNewNode.AfterClone(Me, bCutData, doRoot, oNewNode)
+            Return oNewNode
+        End Function
+
+        Protected Overrides Sub CloneInternal(ByVal doOriginal As AnimatGUI.Framework.DataObject, ByVal bCutData As Boolean, _
+                                            ByVal doRoot As AnimatGUI.Framework.DataObject)
+            MyBase.CloneInternal(doOriginal, bCutData, doRoot)
+
+            Dim doOrig As Bodies.Mesh = DirectCast(doOriginal, Bodies.Mesh)
+
+        End Sub
+
+        Public Overrides Sub BuildProperties(ByRef propTable As AnimatGuiCtrls.Controls.PropertyTable)
+            MyBase.BuildProperties(propTable)
+
+        End Sub
+
+
+        Public Overloads Overrides Sub LoadData(ByRef doStructure As DataObjects.Physical.PhysicalStructure, ByRef oXml As Interfaces.StdXml)
+            MyBase.LoadData(doStructure, oXml)
+
+            oXml.IntoElem() 'Into RigidBody Element
+
+            oXml.OutOfElem() 'Outof RigidBody Element
+
+        End Sub
+
+        Public Overloads Overrides Sub SaveData(ByRef doStructure As DataObjects.Physical.PhysicalStructure, ByRef oXml As Interfaces.StdXml)
+            MyBase.SaveData(doStructure, oXml)
+
+            oXml.IntoElem() 'Into Child Elemement
+
+            oXml.OutOfElem() 'Outof BodyPart Element
+
+        End Sub
+
+        Public Overrides Sub SaveSimulationXml(ByRef oXml As Interfaces.StdXml, Optional ByRef nmParentControl As Framework.DataObject = Nothing, Optional ByVal strName As String = "")
+            MyBase.SaveSimulationXml(oXml, nmParentControl, strName)
+
+            oXml.IntoElem()
+
+            oXml.OutOfElem()
+
+        End Sub
+
+    End Class
+
+
+End Namespace
