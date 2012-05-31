50942ac9876ad194aa4dc285bd10b44867180995
David Cofer <dcofer@NeuroRoboticTech.com>
2012-05-31 22:50:18 +0000

    Added mouth odor eating test files.

---
+++ Libraries/AnimatTesting/UITests/ConversionTests/BodyPartTests/RigidBodyTests/OdorMouthEatingConversionUITest.vb
@@ -0,0 +1,221 @@
+﻿Imports System.Windows.Forms
+Imports Microsoft.VisualStudio.TestTools.UITesting.Keyboard
+Imports System.Runtime.Remoting
+Imports System.Runtime.Remoting.Channels
+Imports System.Runtime.Remoting.Channels.Tcp
+Imports System
+Imports System.CodeDom.Compiler
+Imports System.Configuration
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Text.RegularExpressions
+Imports System.IO
+Imports System.Windows.Input
+Imports Microsoft.VisualStudio.TestTools.UITest.Extension
+Imports Microsoft.VisualStudio.TestTools.UITesting
+Imports Microsoft.VisualStudio.TestTools.UITesting.WinControls
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard
+Imports Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse
+Imports MouseButtons = System.Windows.Forms.MouseButtons
+Imports AnimatTesting.Framework
+Imports System.Xml
+
+Namespace UITests
+    Namespace ConversionTests
+        Namespace BodyPartTests
+            Namespace RigidBodyTests
+
+                <CodedUITest()>
+                Public Class OdorMouthEatingConversionUITest
+                    Inherits ConversionUITest
+
+#Region "Attributes"
+
+
+#End Region
+
+#Region "Properties"
+
+#End Region
+
+#Region "Methods"
+                    '
+
+                    <TestMethod()>
+                    Public Sub Test_OdorMouthEating()
+                        Dim aryMaxErrors As New Hashtable
+                        aryMaxErrors.Add("Time", 0.001)
+                        aryMaxErrors.Add("OdorSensor", 0.5)
+                        aryMaxErrors.Add("Mouth", 0.001)
+                        aryMaxErrors.Add("Eat", 0.05)
+                        aryMaxErrors.Add("Food_Near", 0.05)
+                        aryMaxErrors.Add("default", 0.05)
+
+                        m_strProjectName = "OdorMouthAndEating"
+                        m_strProjectPath = "\Libraries\AnimatTesting\TestProjects\ConversionTests\BodyPartTests\RigidBodyTests"
+                        m_strTestDataPath = "\Libraries\AnimatTesting\TestData\ConversionTests\BodyPartTests\RigidBodyTests\" & m_strProjectName
+                        m_strOldProjectFolder = "\Libraries\AnimatTesting\TestProjects\ConversionTests\OldVersions\BodyPartTests\RigidBodyTests\" & m_strProjectName
+                        m_strStructureGroup = "Organisms"
+                        m_strStruct1Name = "Organism_1"
+
+                        m_aryWindowsToOpen.Clear()
+                        m_aryWindowsToOpen.Add("Tool Viewers\BodyData")
+
+                        'Load and convert the project.
+                        TestConversionProject("AfterConversion_", aryMaxErrors)
+
+                        RunSimulationWaitToEnd()
+                        CompareSimulation(m_strRootFolder & m_strTestDataPath, aryMaxErrors, "AfterConversion_")
+
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\CylinderSensor", "WorldPosition.Y", "-55 c"})
+                        'RunSimulationWaitToEnd()
+                        'CompareSimulation(m_strRootFolder & m_strTestDataPath, aryMaxErrors, "CylinderY_-55cm_", -1, aryIgnoreRows)
+
+                        'aryIgnoreRows.Clear()
+                        'aryIgnoreRows.Add(New Point(131, 137))
+                        'aryIgnoreRows.Add(New Point(323, 324))
+                        'aryIgnoreRows.Add(New Point(399, 400))
+                        'aryIgnoreRows.Add(New Point(529, 530))
+                        'aryIgnoreRows.Add(New Point(2390, 2391))
+                        'aryIgnoreRows.Add(New Point(2496, 2497))
+                        'aryIgnoreRows.Add(New Point(2548, 2549))
+                        'aryIgnoreRows.Add(New Point(2641, 2641))
+
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", "Rotation.X", "-45"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", "WorldPosition.Y", "-10 c"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", "WorldPosition.Z", "-20 c"})
+                        'RunSimulationWaitToEnd()
+                        'CompareSimulation(m_strRootFolder & m_strTestDataPath, aryMaxErrors, "PendulumRotate_", -1, aryIgnoreRows)
+
+                        ''Add subsystem.
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem"}, 2000)
+                        'AddBehavioralNode("Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem", _
+                        '                  "AnimatGUI.DataObjects.Behavior.Nodes.Subsystem", New Point(316, 30), "S2")
+
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\BoxSensor", False})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\BoxContact", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\CylinderSensor", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\CylinderContact", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\BS_BC", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\CS_CC", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\BS_BC\BoxSensor", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\CS_CC\CylinderSensor", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\BoxContact\BS_BC", True})
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\CylinderContact\CS_CC", True})
+                        'DeleteSelectedParts("Delete Group", True)
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2"}, 2000)
+                        'ExecuteMethod("ClickMenuItem", New Object() {"PasteInPlaceToolStripMenuItem"})
+
+
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Tool Viewers\BodyData"}, 2000)
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Tool Viewers\BodyData\LineChart\Y Axis 3", False})
+                        'AddItemToChart("Organism_1\Behavioral System\Neural Subsystem\Nodes\S2\Nodes\BoxContact")
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Tool Viewers\BodyData\LineChart\Y Axis 3\BoxContact", "DataTypeID", "MembraneVoltage"})
+                        'AddItemToChart("Organism_1\Behavioral System\Neural Subsystem\Nodes\S2\Nodes\CylinderContact")
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Tool Viewers\BodyData\LineChart\Y Axis 3\CylinderContact", "DataTypeID", "MembraneVoltage"})
+
+                        'RunSimulationWaitToEnd()
+                        'CompareSimulation(m_strRootFolder & m_strTestDataPath, aryMaxErrors, "PendulumRotate_", -1, aryIgnoreRows)
+
+
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan"}, 2000)
+                        'DeletePart("Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor", "Delete Body Part", True)
+                        'If CBool(ExecuteDirectMethod("DoesObjectExist", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor"})) Then
+                        '    Throw New System.Exception("Sensor was not deleted")
+                        'End If
+                        'If CBool(ExecuteDirectMethod("DoesObjectExist", New Object() {"Tool Viewers\BodyData\LineChart\Y Axis 1\BoxSensor"})) Then
+                        '    Throw New System.Exception("BoxSensor chart node was not removed correctly.")
+                        'End If
+                        'If Not GetSimObjectProperty("Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BoxSensor", "LinkedPart.BodyPart") Is Nothing Then
+                        '    Throw New System.Exception("Linked body part not removed.")
+                        'End If
+                        'If CBool(ExecuteDirectMethod("DoesObjectExist", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BS_BC"})) Then
+                        '    Throw New System.Exception("BS_BC adapter node was not removed correctly.")
+                        'End If
+
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", "Rotation.X", "0"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", "WorldPosition.Y", "-20 c"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", "WorldPosition.Z", "0"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\CylinderSensor", "WorldPosition.Y", "-35 c"})
+
+                        'aryIgnoreRows.Clear()
+                        'aryIgnoreRows.Add(New Point(162, 173))
+                        'aryIgnoreRows.Add(New Point(405, 406))
+                        'aryIgnoreRows.Add(New Point(512, 515))
+                        'aryIgnoreRows.Add(New Point(2405, 2407))
+                        'aryIgnoreRows.Add(New Point(2491, 2494))
+                        'aryIgnoreRows.Add(New Point(2628, 2632))
+
+                        'RunSimulationWaitToEnd()
+                        'CompareSimulation(m_strRootFolder & m_strTestDataPath, aryMaxErrors, "DeleteBoxSensor_", -1, aryIgnoreRows)
+
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan"}, 2000)
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\CylinderSensor", "WorldPosition.X", "5 c"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\CylinderSensor", "WorldPosition.Y", "-35 c"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\CylinderSensor", "WorldPosition.Z", "0"})
+
+                        'PasteChildPartTypeWithJoint("", "Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum", 0.04, 0.55, -0.5, 0.0, 0.0, -1.0, False)
+                        'Threading.Thread.Sleep(1000)
+
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor", "WorldPosition.X", "-5 c"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor", "WorldPosition.Y", "-35 c"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor", "WorldPosition.Z", "0"})
+
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Tool Viewers\BodyData"}, 2000)
+                        'ExecuteMethod("SelectWorkspaceItem", New Object() {"Tool Viewers\BodyData\LineChart\Y Axis 1", False})
+                        'AddItemToChart("Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor")
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Tool Viewers\BodyData\LineChart\Y Axis 1\BoxSensor", "DataTypeID", "ContactCount"})
+
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2"}, 2000)
+                        'ExecuteMethod("SetLinkedItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BoxSensor", _
+                        '              "Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor"})
+
+                        'AddBehavioralLink("Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BoxSensor", _
+                        '                  "Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BoxContact", "", "", False)
+                        'If Not CBool(ExecuteDirectMethod("DoesObjectExist", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\2"})) Then
+                        '    Throw New System.Exception("2 adapter was not added")
+                        'End If
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\2", "Name", "BS_BC"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BS_BC", "Gain.C", "10 n"})
+                        'ExecuteMethod("SetObjectProperty", New Object() {"Simulation\Environment\Organisms\Organism_1\Behavioral System\Neural Subsystem\S2\BS_BC", "DataTypes", "AnimatGUI.DataObjects.Physical.Bodies.Box.DataTypes.ContactCount"})
+
+                        'RunSimulationWaitToEnd()
+                        'CompareSimulation(m_strRootFolder & m_strTestDataPath, aryMaxErrors, "AfterConversion_", -1, aryIgnoreRows)
+
+                        ''Click 'Add Part' button
+                        'ExecuteMethod("DblClickWorkspaceItem", New Object() {"Simulation\Environment\Organisms\Organism_1\Body Plan"}, 2000)
+                        'ExecuteMethod("ClickToolbarItem", New Object() {"AddPartToolStripButton"}, 2000)
+                        'AutomatedClickToAddBody("Simulation\Environment\Organisms\Organism_1\Body Plan\Root\Hinge\Pendulum\BoxSensor", 0.04, 0.55, -0.5, 0.0, 0.0, -1.0)
+                        'AssertErrorDialogShown("You cannot add children to a contact sensor class.", enumMatchType.Equals)
+
+                    End Sub
+
+
+#Region "Additional test attributes"
+                    '
+                    ' You can use the following additional attributes as you write your tests:
+                    '
+                    ' Use TestInitialize to run code before running each test
+                    <TestInitialize()> Public Overrides Sub MyTestInitialize()
+                        MyBase.MyTestInitialize()
+
+                        'This test compares data to that generated from the old version. We never re-generate the data in V2, so this should always be false 
+                        'regardless of the setting in app.config.
+                        m_bGenerateTempates = False
+
+                    End Sub
+
+                    <TestCleanup()> Public Overrides Sub MyTestCleanup()
+                        MyBase.MyTestCleanup()
+                    End Sub
+#End Region
+
+#End Region
+
+                End Class
+
+            End Namespace
+        End Namespace
+    End Namespace
+End Namespace
