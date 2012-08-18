065ce2cd1df8753b1f2f0750b97a0459a0946835
David Cofer <dcofer@NeuroRoboticTech.com>
2012-08-18 21:37:16 +0000

    Added FreeJoint type

---
+++ Libraries/AnimatGUI/DataObjects/Physical/Joints/FreeJoint.vb
@@ -0,0 +1,98 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Xml
+Imports AnimatGuiCtrls.Controls
+Imports AnimatGUI.Framework
+
+Namespace DataObjects.Physical.Joints
+
+    Public Class FreeJoint
+        Inherits Physical.Joint
+
+#Region " Attributes "
+
+
+#End Region
+
+#Region " Properties "
+
+        Public Overrides ReadOnly Property WorkspaceImageName() As String
+            Get
+                Return "AnimatGUI.FreeJoint_Treeview.gif"
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property ButtonImageName() As String
+            Get
+                Return "AnimatGUI.FreeJiont_SelectType.gif"
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property Type() As String
+            Get
+                Return "FreeJoint"
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property PartType() As System.Type
+            Get
+                Return GetType(AnimatGUI.DataObjects.Physical.Joints.FreeJoint)
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property UsesRadians() As Boolean
+            Get
+                Return False
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property CanBeCharted() As Boolean
+            Get
+                Return False
+            End Get
+        End Property
+
+        <Browsable(False)> _
+        Public Overrides ReadOnly Property ModuleName() As String
+            Get
+                Return "VortexAnimatPrivateSim_VC" & Util.Application.SimVCVersion & Util.Application.RuntimeModePrefix & ".dll"
+            End Get
+        End Property
+
+#End Region
+
+        Public Sub New(ByVal doParent As Framework.DataObject)
+            MyBase.New(doParent)
+            m_strDescription = "Allows the joint to move freely in all 6 DOF."
+
+        End Sub
+
+        Public Overrides Sub SetDefaultSizes()
+            MyBase.SetDefaultSizes()
+            m_snSize.ActualValue = 0.05 * Util.Environment.DistanceUnitValue
+        End Sub
+
+        Public Overrides Function Clone(ByVal doParent As Framework.DataObject, ByVal bCutData As Boolean, ByVal doRoot As Framework.DataObject) As Framework.DataObject
+            Dim oNewNode As New Joints.FreeJoint(doParent)
+            oNewNode.CloneInternal(Me, bCutData, doRoot)
+            If Not doRoot Is Nothing AndAlso doRoot Is Me Then oNewNode.AfterClone(Me, bCutData, doRoot, oNewNode)
+            Return oNewNode
+        End Function
+
+        Protected Overrides Sub CloneInternal(ByVal doOriginal As AnimatGUI.Framework.DataObject, ByVal bCutData As Boolean, _
+                                            ByVal doRoot As AnimatGUI.Framework.DataObject)
+            MyBase.CloneInternal(doOriginal, bCutData, doRoot)
+
+            Dim doOrig As Joints.FreeJoint = DirectCast(doOriginal, Joints.FreeJoint)
+
+        End Sub
+
+    End Class
+
+
+End Namespace
