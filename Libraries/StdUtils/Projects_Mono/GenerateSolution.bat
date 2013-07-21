8bbedf19e9c74937fbe7a517819ef3c3ed5bccfc
David Cofer <dcofer@NeuroRoboticTech.com>
2013-07-21 09:21:56 -0500

    regeneraterd these project files for linux

--- Libraries/StdUtils/Projects_Mono/GenerateSolution.bat
+++ Libraries/StdUtils/Projects_Mono/GenerateSolution.bat
@@ -4,5 +4,7 @@ premake4m --os=linux --file=Solution.lua monodevelop
 ..\..\..\bin\fart *.cproj "<Include>..\..\..\..\include</Include>" "<Include>../../../../include</Include>"
 ..\..\..\bin\fart *.cproj "<LibPath>..\..\..\..\lib</LibPath>" "<LibPath>../../../../lib</LibPath>"
 ..\..\..\bin\fart *.cproj "<Include>..\..\..\StdUtils</Include>" "<Include>../../../StdUtils</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\..\..\..\..\3rdParty\boost_1_54_0</Include>" "<Include>../../../../../3rdParty/boost_1_54_0</Include>"
+..\..\..\bin\fart *.cproj "<LibPath>..\..\..\..\..\3rdParty\boost_1_54_0\lib</LibPath>" "<LibPath>../../../../../3rdParty/boost_1_54_0/lib</LibPath>"
 
 @pause
