53e8bbd21db1c8bf58bee98b8f6a2f7049ed7977
David Cofer <dcofer@NeuroRoboticTech.com>
2013-08-03 08:01:32 -0500

    Modified VortexAnimatSim project to compile on linux

--- Libraries/VortexAnimatSim/Projects_Mono/GenerateSolution.bat
+++ Libraries/VortexAnimatSim/Projects_Mono/GenerateSolution.bat
@@ -12,6 +12,7 @@ premake4m --os=linux --file=Solution.lua monodevelop
 ..\..\..\bin\fart *.cproj "<Include>..\..\..\Vortex_5_1\3rdparty\osg-2.8.3\include</Include>" "<Include>../../../Vortex_5_1/3rdparty/osg-2.8.3/include</Include>"
 ..\..\..\bin\fart *.cproj "<Include>..\..\..\Vortex_5_1\3rdparty\sdl-1.2.14\include</Include>" "<Include>../../../Vortex_5_1/3rdparty/sdl-1.2.14/include</Include>"
 
+..\..\..\bin\fart *.cproj "<Include>..\..\..\OsgAnimatSim</Include>" "<Include>../../../OsgAnimatSim</Include>"
 ..\..\..\bin\fart *.cproj "<LibPath>..\..\..\Vortex_5_1\3rdparty\osg-2.8.3\lib</LibPath>" "<LibPath>../../../Vortex_5_1/3rdparty/osg-2.8.3/lib</LibPath>"
 ..\..\..\bin\fart *.cproj "<LibPath>..\..\..\Vortex_5_1\lib</LibPath>" "<LibPath>../../../Vortex_5_1/lib</LibPath>"
 ..\..\..\bin\fart *.cproj "<LibPath>..\..\..\Vortex_5_1\3rdparty\boost-1.45.0\lib</LibPath>" "<LibPath>../../../Vortex_5_1/3rdparty/boost-1.45.0/lib</LibPath>"
