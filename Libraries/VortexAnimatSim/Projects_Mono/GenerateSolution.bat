1a42eefcd0a4a5841ea78860ffa449de27a74e93
David Cofer <dcofer@NeuroRoboticTech.com>
2013-08-23 07:27:50 -0500

    Forgot to modify the project files for mono

--- Libraries/VortexAnimatSim/Projects_Mono/GenerateSolution.bat
+++ Libraries/VortexAnimatSim/Projects_Mono/GenerateSolution.bat
@@ -18,6 +18,19 @@ premake4m --os=linux --file=Solution.lua monodevelop
 ..\..\..\bin\fart *.cproj "<LibPath>..\..\..\Vortex_5_1\lib</LibPath>" "<LibPath>../../../Vortex_5_1/lib</LibPath>"
 ..\..\..\bin\fart *.cproj "<LibPath>..\..\..\Vortex_5_1\3rdparty\boost-1.45.0\lib</LibPath>" "<LibPath>../../../Vortex_5_1/3rdparty/boost-1.45.0/lib</LibPath>"
 
-..\..\..\bin\fart *.cproj "<LibPath>..\..\..\..\..\3rdParty\boost_1_54_0\lib</LibPath>" "<LibPath>../../../../../3rdParty/boost_1_54_0/lib</LibPath>"
+..\..\..\bin\fart *.cproj "<Include>..\include</Include>" "<Include>../include</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\Libraries\Vortex_5_1\include</Include>" "<Include>../Libraries/Vortex_5_1/include</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\Libraries\Vortex_5_1\3rdparty\osg-2.8.3\include</Include>" "<Include>../Libraries/Vortex_5_1/3rdparty/osg-2.8.3/include</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\Libraries\StdUtils</Include>" "<Include>../Libraries/StdUtils</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\Libraries\AnimatSim</Include>" "<Include>../Libraries/AnimatSim</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\Libraries\OsgAnimatSim</Include>" "<Include>../Libraries/OsgAnimatSim</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\Libraries\VortexAnimatSim</Include>" "<Include>../Libraries/VortexAnimatSim</Include>"
+..\..\..\bin\fart *.cproj "<Include>..\..\3rdParty\boost_1_54_0</Include>" "<Include>../../3rdParty/boost_1_54_0</Include>"
+
+..\..\..\bin\fart *.cproj "<LibPath>..\Libraries\Vortex_5_1\3rdparty\osg-2.8.3\lib</LibPath>" "<LibPath>../Libraries/Vortex_5_1/3rdparty/osg-2.8.3/lib</LibPath>"
+..\..\..\bin\fart *.cproj "<LibPath>..\Libraries\Vortex_5_1\lib</LibPath>" "<LibPath>../Libraries/Vortex_5_1/lib</LibPath>"
+..\..\..\bin\fart *.cproj "<LibPath>..\Libraries\Vortex_5_1\3rdparty\boost-1.45.0\lib</LibPath>" "<LibPath>../Libraries/Vortex_5_1/3rdparty/boost-1.45.0/lib</LibPath>"
+..\..\..\bin\fart *.cproj "<LibPath>..\..\3rdParty\boost_1_54_0\lib</LibPath>" "<LibPath>../../3rdParty/boost_1_54_0/lib</LibPath>"
+
 
 @pause
