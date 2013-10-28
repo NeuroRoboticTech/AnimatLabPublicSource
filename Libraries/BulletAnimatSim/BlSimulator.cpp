1355942694ad2c2c4d84b9e7bfb77c989c79975c
David Cofer <dcofer@NeuroRoboticTech.com>
2013-10-28 07:24:48 -0500

    Added some more test projects. I have a weird one where a unit conversion is producing bad behavior, but if I create it from scratch, or if I move one of the parts it behaves correctly. Happens at all unit scales.

--- Libraries/BulletAnimatSim/BlSimulator.cpp
+++ Libraries/BulletAnimatSim/BlSimulator.cpp
@@ -49,7 +49,7 @@ BlSimulator::BlSimulator()
 	if(!m_lpAnimatClassFactory) 
 		m_lpAnimatClassFactory = new BlClassFactory;
 
-    m_bDrawDebug = false;
+    m_bDrawDebug = true;
 }
 
 BlSimulator::~BlSimulator()
