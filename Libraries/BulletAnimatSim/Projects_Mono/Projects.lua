ad75fa6b24f2c7f4b79a72135fcd149ecbd90033
David Cofer <dcofer@NeuroRoboticTech.com>
2014-03-24 07:46:22 -0500

    Modified the bullet animatsim project

--- Libraries/BulletAnimatSim/Projects_Mono/Projects.lua
+++ Libraries/BulletAnimatSim/Projects_Mono/Projects.lua
@@ -4,8 +4,7 @@
 		kind     "SharedLib"
 		files  { "../*.h",
 			 "../*.cpp"}
-		includedirs { "../../../../include", 
-			      "../../../../../3rdParty/OpenSceneGraph-3.0.1/include",
+		includedirs { "../../../../include",
 			      "../../../../../3rdParty/osgWorks_03_00_00/include",
 			      "../../../../../3rdParty/Bullet-2.82/src",
 			      "../../../../../3rdParty/osgBullet_03_00_00/include",
@@ -13,10 +12,7 @@
 		   	      "../../../AnimatSim",
 			      "../../../OsgAnimatSim" }
 		libdirs { "../../../../bin",
-				  "../../../../../3rdParty/OpenSceneGraph-3.0.1/bin",
-				  "../../../../../3rdParty/osgWorks_03_00_00/bin",
-				  "../../../../../3rdParty/Bullet-2.82/bin",
-				  "../../../../../3rdParty/osgBullet_03_00_00/bin" }
+			  "../../../../../3rdParty/osgBullet_03_00_00/bin/lib/x86_64-linux-gnu" }
 		links { "dl" }
 	  
 		configuration { "Debug", "linux" }
@@ -27,31 +23,24 @@
 			links { "StdUtils_vc10D", 
 				"AnimatSim_vc10D",
 				"OsgAnimatSim_vc10D",
-				"OpenThreadsd", 
-				"osgAnimationd", 
-				"osgd", 
-				"osgDBd", 
-				"osgFXd", 
-				"osgGAd", 
-				"osgManipulatord", 
-				"osgParticled", 
-				"osgShadowd", 
-				"osgSimd", 
-				"osgTerraind", 
-				"osgTextd", 
-				"osgUtild", 
-				"osgViewerd", 
-				"osgVolumed", 
-				"osgWidgetd",
-				"osgbDynamicsd",
-				"osgbCollisiond",
-				"osgwControlsd",
-				"osgwQueryd",
-				"osgwToolsd",
-				"BulletDynamics_single_debug",
-				"BulletCollision_single_debug",
-				"LinearMath_single_debug",
-				"BulletSoftBody_single_debug"}
+				"OpenThreads", 
+				"osgAnimation", 
+				"osg", 
+				"osgDB", 
+				"osgFX", 
+				"osgGA", 
+				"osgManipulator", 
+				"osgParticle", 
+				"osgShadow", 
+				"osgSim", 
+				"osgTerrain", 
+				"osgText", 
+				"osgUtil", 
+				"osgViewer", 
+				"osgVolume", 
+				"osgWidget",
+				"osgbDynamics_single_debug",
+				"osgbCollision_single_debug"}
 			postbuildcommands { "cp Debug/libBulletAnimatSim_vc10D_single.so ../../../bin" }
 
 		configuration { "Debug_Double", "linux" }
@@ -62,31 +51,24 @@
 			links { "StdUtils_vc10D", 
 				"AnimatSim_vc10D",
 				"OsgAnimatSim_vc10D",
-				"OpenThreadsd", 
-				"osgAnimationd", 
-				"osgd", 
-				"osgDBd", 
-				"osgFXd", 
-				"osgGAd", 
-				"osgManipulatord", 
-				"osgParticled", 
-				"osgShadowd", 
-				"osgSimd", 
-				"osgTerraind", 
-				"osgTextd", 
-				"osgUtild", 
-				"osgViewerd", 
-				"osgVolumed", 
-				"osgWidgetd",
-				"osgbDynamicsd",
-				"osgbCollisiond",
-				"osgwControlsd",
-				"osgwQueryd",
-				"osgwToolsd",
-				"BulletDynamics_double_debug",
-				"BulletCollision_double_debug",
-				"LinearMath_double_debug",
-				"BulletSoftBody_double_debug"}
+				"OpenThreads", 
+				"osgAnimation", 
+				"osg", 
+				"osgDB", 
+				"osgFX", 
+				"osgGA", 
+				"osgManipulator", 
+				"osgParticle", 
+				"osgShadow", 
+				"osgSim", 
+				"osgTerrain", 
+				"osgText", 
+				"osgUtil", 
+				"osgViewer", 
+				"osgVolume", 
+				"osgWidget",
+				"osgbDynamics_double_debug",
+				"osgbCollision_double_debug" }
 			postbuildcommands { "cp Debug/libBulletAnimatSim_vc10D_double.so ../../../bin" }
 			
 		configuration { "Release", "linux" }
@@ -113,15 +95,36 @@
 				"osgViewer",
 				"osgVolume",
 				"osgWidget",
-				"osgbDynamics",
-				"osgbCollision",
-				"osgwControls",
-				"osgwQuery",
-				"osgwTools",
-				"BulletDynamics",
-				"BulletCollision",
-				"LinearMath",
-				"BulletSoftBody"}					
+				"osgbDynamics_single",
+				"osgbCollision_single"}					
 			postbuildcommands { "cp Release/libBulletAnimatSim_vc10.so ../../../bin", 
 					    "cp Release/libBulletAnimatSim_vc10.so ../../../unit_test_bin" }
+
+		configuration { "Release_Double", "linux" }
+			defines { "_DEBUG", "OSGBULLET_STATIC", "BULLETANIMATLIBRARY_EXPORTS", "BT_USE_DOUBLE_PRECISION" }
+			flags   { "Symbols", "SEH" }
+			targetdir ("Release")
+			targetname ("BulletAnimatSim_vc10_double")
+			links { "StdUtils_vc10", 
+				"AnimatSim_vc10",
+				"OsgAnimatSim_vc10",
+				"OpenThreads", 
+				"osgAnimation", 
+				"osg", 
+				"osgDB", 
+				"osgFX", 
+				"osgGA", 
+				"osgManipulator", 
+				"osgParticle", 
+				"osgShadow", 
+				"osgSim", 
+				"osgTerrain", 
+				"osgText", 
+				"osgUtil", 
+				"osgViewer", 
+				"osgVolume", 
+				"osgWidget",
+				"osgbDynamics_double",
+				"osgbCollision_double" }
+			postbuildcommands { "cp Debug/libBulletAnimatSim_vc10_double.so ../../../bin" }
 
--- Libraries/BulletAnimatSim/Projects_Mono/Solution.lua
+++ Libraries/BulletAnimatSim/Projects_Mono/Solution.lua
@@ -1,6 +1,5 @@
 -- A solution contains projects, and defines the available configurations
 solution "BulletAnimatSim"
 	configurations { "Debug", "Release", "Debug_Double", "Release_Double" }
-	platforms {"x32"}
 	dofile "Projects.lua"
-	dofile "UnitTests.lua"
+
