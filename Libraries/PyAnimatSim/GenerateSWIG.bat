26972b76974ae66d17f40329453c228cf01545cf
David Cofer <dcofer@NeuroRoboticTech.com>
2014-05-20 09:47:26 -0500

    Renaming PyAnimatSim to AnimatSimPy to conform to python naming convention.

--- Libraries/PyAnimatSim/GenerateSWIG.bat
+++
@@ -1,5 +0,0 @@
-swig -c++ -python PyAnimatSim.i 
-
-rem del "..\..\AnimatSimCSharpInterface\*.cs"
-
-rem xcopy "*.cs" "..\..\AnimatSimCSharpInterface" /E /Y /R
--- Libraries/PyAnimatSim/Projects_VisualStudio/GenerateSolution.bat
+++
@@ -1,5 +0,0 @@
-echo "Generating PyAnimatSim Visual Studio Solution"
-
-..\..\..\bin\premake4 --os=windows --file=PyAnimatSim.lua vs2010
-
-@pause
