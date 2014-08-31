5d71c290fe5263e58a9e9ba7ae94ce9d55c5a6a9
David Cofer <dcofer@NeuroRoboticTech.com>
2014-08-31 11:26:54 -0500

    Modification to allow it to compile on ubuntu 13. Will need to fix this so it pulls the right boost/atomic header based on boost version.

--- include/StdUtils.h
+++ include/StdUtils.h
@@ -91,7 +91,7 @@
 	#include <boost/interprocess/sync/interprocess_mutex.hpp>
 	#include <boost/interprocess/sync/interprocess_condition.hpp>
 	#include <boost/interprocess/sync/scoped_lock.hpp>
-	#include <boost/atomic.hpp>
+	#include <boost/interprocess/detail/atomic.hpp>
 #endif
 
 #ifdef WIN32
