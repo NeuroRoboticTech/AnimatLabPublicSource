58ad668eb83b1ad1d0aaa1d4bac7aa56d3700050
David Cofer <dcofer@NeuroRoboticTech.com>
2014-10-13 14:27:30 -0500

    Got the NodeToNodeAdapter working.

--- Libraries/AnimatCarlGUI/DataObjects/Behavior/NodeTypes/NodeToNodeAdapter.vb
+++ Libraries/AnimatCarlGUI/DataObjects/Behavior/NodeTypes/NodeToNodeAdapter.vb
@@ -7,6 +7,7 @@ Imports System.Diagnostics
 Imports System.IO
 Imports System.Xml
 Imports AnimatGuiCtrls.Controls
+Imports AnimatGUI.Framework
 
 Namespace DataObjects.Behavior.NodeTypes
 
@@ -14,6 +15,10 @@ Namespace DataObjects.Behavior.NodeTypes
         Inherits AnimatGUI.DataObjects.Behavior.Nodes.NodeToNodeAdapter
 
 #Region " Attributes "
+
+        Protected m_eCoverage As enumCoverage = enumCoverage.WholePopulation
+
+        Protected m_aryCellsToStim As New Collections.NeuralIndices(Me)
 
 #End Region
 
@@ -23,6 +28,42 @@ Namespace DataObjects.Behavior.NodeTypes
             Get
                 Return "AnimatCarlSimCUDA"
             End Get
+        End Property
+
+        <Browsable(False)> _
+        Public Overridable Property Coverage() As enumCoverage
+            Get
+                Return m_eCoverage
+            End Get
+            Set(value As enumCoverage)
+                SetSimData("Coverage", value.ToString(), True)
+                m_eCoverage = value
+
+                If Not Util.ProjectWorkspace Is Nothing Then
+                    Util.ProjectWorkspace.RefreshProperties()
+                End If
+            End Set
+        End Property
+
+        Public Overridable Property CellsToStim As Collections.NeuralIndices
+            Get
+                Return m_aryCellsToStim
+            End Get
+            Set(value As Collections.NeuralIndices)
+                'Set nothing here. It is set in the property editor
+
+                'We do need to reset the list of indices in the simulator though
+                Dim oXml As ManagedAnimatInterfaces.IStdXml = Util.Application.CreateStdXml()
+                oXml.AddElement("Root")
+                oXml.AddChildElement("Cells")
+                oXml.IntoElem()
+                For Each iIdx As Integer In m_aryCellsToStim
+                    oXml.AddChildElement("Cell", iIdx)
+                Next
+                oXml.IntoElem()
+
+                SetSimData("CellsToStim", oXml.Serialize(), True)
+            End Set
         End Property
 
 #End Region
@@ -44,6 +85,21 @@ Namespace DataObjects.Behavior.NodeTypes
             Return oNewNode
         End Function
 
+        Protected Overrides Sub CloneInternal(doOriginal As AnimatGUI.Framework.DataObject, bCutData As Boolean, doRoot As AnimatGUI.Framework.DataObject)
+            MyBase.CloneInternal(doOriginal, bCutData, doRoot)
+
+            Dim doRate As NodeToNodeAdapter = DirectCast(doOriginal, NodeToNodeAdapter)
+
+            m_eCoverage = doRate.m_eCoverage
+            m_aryCellsToStim = DirectCast(doRate.m_aryCellsToStim.Clone(Me, bCutData, doRoot), Collections.NeuralIndices)
+        End Sub
+
+        Public Overrides Sub ClearIsDirty()
+            MyBase.ClearIsDirty()
+
+            m_aryCellsToStim.ClearIsDirty()
+        End Sub
+
         Protected Overrides Sub SaveSimulationXmlInternal(oXml As ManagedAnimatInterfaces.IStdXml, nmSource As AnimatGUI.DataObjects.Behavior.NeuralModule, _
                                                           nmTarget As AnimatGUI.DataObjects.Behavior.NeuralModule, bnOrigin As AnimatGUI.DataObjects.Behavior.Node, _
                                                           bnDestination As AnimatGUI.DataObjects.Behavior.Node, Optional ByRef nmParentControl As AnimatGUI.Framework.DataObject = Nothing, _
@@ -51,10 +107,87 @@ Namespace DataObjects.Behavior.NodeTypes
             MyBase.SaveSimulationXmlInternal(oXml, nmSource, nmTarget, bnOrigin, bnDestination, nmParentControl, strName)
 
             oXml.IntoElem()
+
+            oXml.AddChildElement("Coverage", m_eCoverage.ToString())
+
+            If m_eCoverage = enumCoverage.Individuals AndAlso m_aryCellsToStim.Count > 0 Then
+                oXml.AddChildElement("Cells")
+                oXml.IntoElem()  'Into Cells
+
+                For Each iCell As Integer In m_aryCellsToStim
+                    oXml.AddChildElement("Cell", iCell)
+                Next
+
+                oXml.OutOfElem() 'Outof Cells
+            End If
+
             oXml.OutOfElem()
         End Sub
 
 #Region " DataObject Methods "
+
+        Public Overrides Sub BuildProperties(ByRef propTable As AnimatGuiCtrls.Controls.PropertyTable)
+            MyBase.BuildProperties(propTable)
+
+            propTable.Properties.Add(New AnimatGuiCtrls.Controls.PropertySpec("Coverage", m_eCoverage.GetType(), "Coverage", _
+                                        "Adapter Properties", "Determines whether we apply this stimulus to the entire population or individual cells.", m_eCoverage))
+
+            If m_eCoverage = enumCoverage.Individuals Then
+                Dim pbNumberBag As AnimatGuiCtrls.Controls.PropertyBag = m_aryCellsToStim.Properties
+                propTable.Properties.Add(New AnimatGuiCtrls.Controls.PropertySpec("Cells To Stimulate", pbNumberBag.GetType(), "CellsToStim", _
+                                            "Adapter Properties", "The list of cell indices to stimulate.", pbNumberBag, _
+                                            GetType(TypeHelpers.SelectedIndexEditor), GetType(AnimatGuiCtrls.Controls.ExpandablePropBagConverter)))
+            End If
+
+        End Sub
+
+        Public Overrides Sub SaveData(oXml As ManagedAnimatInterfaces.IStdXml)
+            MyBase.SaveData(oXml)
+
+            oXml.IntoElem()
+
+            oXml.AddChildElement("Coverage", m_eCoverage.ToString)
+
+            If m_eCoverage = enumCoverage.Individuals AndAlso m_aryCellsToStim.Count > 0 Then
+                oXml.AddChildElement("Cells")
+                oXml.IntoElem()  'Into Cells
+
+                For Each iCell As Integer In m_aryCellsToStim
+                    oXml.AddChildElement("Cell", iCell)
+                Next
+
+                oXml.OutOfElem() 'Outof Cells
+            End If
+
+            oXml.OutOfElem() ' Outof Node Element
+
+        End Sub
+
+        Public Overrides Sub LoadData(oXml As ManagedAnimatInterfaces.IStdXml)
+            MyBase.LoadData(oXml)
+
+            oXml.IntoElem()
+
+            m_eCoverage = DirectCast([Enum].Parse(GetType(enumCoverage), oXml.GetChildString("Coverage", "WholePopulation"), True), enumCoverage)
+
+            m_aryCellsToStim.Clear()
+            If oXml.FindChildElement("Cells", False) Then
+                oXml.IntoElem()
+
+                Dim iCount As Integer = oXml.NumberOfChildren() - 1
+                For iIdx As Integer = 0 To iCount
+                    oXml.FindChildByIndex(iIdx)
+                    Dim iNeuronIdx As Integer = oXml.GetChildInt()
+                    m_aryCellsToStim.Add(iNeuronIdx)
+                Next
+
+                oXml.OutOfElem()
+            End If
+
+            oXml.OutOfElem()
+
+            Me.IsDirty = False
+        End Sub
 
 #End Region
 
